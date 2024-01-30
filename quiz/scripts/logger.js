9190695a28b7cddd74ca56d9be59a811425e0913
Thean Lim <lim.the@northeastern.edu>
2024-01-30 12:40:35 -0500

    Implement logMsg() and 'call' a function by passing objects

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,15 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+function logMsg(){
+    console.log(`My Error Log: ${this.errMsg}`);
+}
+
+const o1 = {
+    errMsg: "Error Message 1"
+}
+
+const o2 = {
+    errMsg: "Error Message 2"
+}
+
+console.log(logMsg.call(o1));
+console.log(logMsg.call(o2));
